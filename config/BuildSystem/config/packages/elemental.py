431986fe8d027fa0ce079273efe91eb7c0888028
Satish Balay <balay@mcs.anl.gov>
2016-11-23 19:29:56 -0600

    elemental: v0.87.4 - fixes for valgrind memleaks and compiler unused variable warnings

--- config/BuildSystem/config/packages/elemental.py
+++ config/BuildSystem/config/packages/elemental.py
@@ -3,8 +3,8 @@ import config.package
 class Configure(config.package.CMakePackage):
   def __init__(self, framework):
     config.package.CMakePackage.__init__(self, framework)
-    self.gitcommit        = 'v0.87'
-    self.download          = ['git://https://github.com/elemental/Elemental']
+    self.gitcommit        = 'v0.87.4'
+    self.download         = ['git://https://github.com/elemental/Elemental']
     self.liblist          = [['libEl.a','libElSuiteSparse.a','libpmrrr.a']]
     self.includes         = ['El.hpp']
     self.precisions       = ['single','double']

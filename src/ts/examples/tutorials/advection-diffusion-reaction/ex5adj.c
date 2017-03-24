8b8a60a6f2ce0e737a85f4fa55a3cb4387cb0c4f
Satish Balay <balay@mcs.anl.gov>
2017-03-24 18:56:02 -0500

    Revert "Merge remote-tracking branch 'origin/hongzh/knl' into next"
    This reverts commit 4c478c4f59e90f1b4cb0e534e6f4a9d7c0d2b4a6, reversing
    changes made to 59165b1aecfe15ba7d7a911ec12983d46d0139f4.

--- src/ts/examples/tutorials/advection-diffusion-reaction/ex5adj.c
+++ src/ts/examples/tutorials/advection-diffusion-reaction/ex5adj.c
@@ -130,6 +130,7 @@ int main(int argc,char **argv)
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     ierr = VecDuplicate(x,&lambda[0]);CHKERRQ(ierr);
     /*   Reset initial conditions for the adjoint integration */
+    ierr = VecGetArray(lambda[0],&x_ptr);CHKERRQ(ierr);
     ierr = InitializeLambda(da,lambda[0],0.5,0.5);CHKERRQ(ierr);
 
     ierr = TSSetCostGradients(ts,1,lambda,NULL);CHKERRQ(ierr);
@@ -145,7 +146,8 @@ int main(int argc,char **argv)
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = TSDestroy(&ts);CHKERRQ(ierr);
   ierr = DMDestroy(&da);CHKERRQ(ierr);
-  ierr = PetscFinalize();
+  ierr = PetscFinalize(); if(ierr) return ierr;
+  ierr = PetscMallocClear();
   return ierr;
 }
 
